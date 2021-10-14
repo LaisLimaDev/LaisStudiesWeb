75802e2465f07fdb291407771399346b4d2e9502
LaisLimaSantos <laissp25@gmail.com>
2021-10-13 23:49:34 -0300

    backup

---
+++ testes diversos/testes.js
@@ -0,0 +1,14 @@
+/** revisão operadores de atribuição e valores booleanos ***/
+if (0 == false )console.log(false);
+
+0 === false;
+1 == "1";
+1 === "1" ;
+'0' === false;
+
+string: var name = 'Luna'; var age = 2
+console.log('Meu cachorro ' + name + ' tem ' + age + ' anos de idade e '+ age * 7 +' se comparado com a idade humana.'); 
+console.log(`Meu cachorro ${name} tem ${age} anos de idade e ${age * 7} se comparado com a idade humana.`); 
+console.log('Hello ${name}! How are you today?');
+console.log(`Hello' + name+ '! How are you today?`); 
+console.log('Hello ' + name + '\n'+ 'How are you'+ '\n'+ 'today?');
